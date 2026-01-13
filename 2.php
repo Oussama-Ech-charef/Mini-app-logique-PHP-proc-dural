f85242e90cde2921643b11e3867bb8f8835e7aa0
OUSSAMA <oussamacharef71@gmail.com>
2026-01-13 13:14:46 +0100

    add chapiter 2

---
+++ 2.php
@@ -0,0 +1,35 @@
+<?php
+
+
+
+
+$age = 17 ;
+if ($age >= 18) {
+    echo "Vous êtes majeur.";
+} else {
+    echo "Vous êtes mineur.";
+}
+
+
+$note = 14;
+
+if ($note >= 16) {
+    echo "Très bien";
+
+}elseif ($note >= 10) {
+    echo "passable";
+} else {
+    echo "Échec";
+} 
+
+$jour = "Vendredi";
+switch ($jour) {
+    case "Landi":
+        echo "Début de semaine";
+        break;
+    case "Vendredi":
+        echo "Dernier jour avant le week-end";
+        break;
+    default:
+        echo "Jour normal";
+}
