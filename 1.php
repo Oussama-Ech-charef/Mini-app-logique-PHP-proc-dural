283d5932e20c072b5fa1eabf98f1dae981d0e060
OUSSAMA <oussamacharef71@gmail.com>
2026-01-13 12:06:26 +0100

    add chapiter 1

---
+++ 1.php
@@ -0,0 +1,35 @@
+<?php
+$nom = "oussama";
+$age = 22;
+$prix = 19.99;
+$estConnecte = true;
+
+
+
+
+
+
+
+echo "Nom : " . $nom . "<br>";
+
+echo "Age : " .$age . "<br>";
+
+echo "Prix : " . $prix . "€<br>";
+
+echo "Connecté : " .($estConnecte ? "Oui" : "Non") . "<br>";
+
+
+var_dump($nom);
+
+
+
+
+
+
+
+$nombre = "100";
+
+$nombreInt = (int)$nombre;
+
+echo "Conversion : " . $nombreInt;
+
