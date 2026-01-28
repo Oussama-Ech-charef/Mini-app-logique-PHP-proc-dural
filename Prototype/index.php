0c5acf3d912416f992b9302b211293744081da02
OUSSAMA <oussamacharef71@gmail.com>
2026-01-28 22:41:10 +0100

    add modification to prototype

--- Prototype/index.php
+++ Prototype/index.php
@@ -1,123 +1,31 @@
-
-
 <!DOCTYPE html>
 <html lang="en">
 <head>
    <meta charset="UTF-8">
    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-   <title>Prototype</title>
-   
+   <title>Calculatrice PHP Simple</title>
 </head>
 <body>
+   <h1>Ma Calculatrice PHP</h1>
 
-   
-   <h1> Simple PHP Calculator</h1>
+   <form  method="POST"  action="">
+      <input type="number" name="n1" placeholder="Nombre 1" required>
 
-   <form action="#" method="post">
+      <select name="operation">
+         <option value="+">+</option>
+         <option value="-">-</option>
+         <option value="*">*</option>
+         <option value="/">/</option>
+      </select>
 
-      <div class="input">
-          <input type="number" require name="number_one" placeholder="Value:A">
-          <br>
-          <br>
-          <input type="number" require  name="number_tow" placeholder="Value:B">
-      </div>
+      <input type="number" name="n2" placeholder="Numbre 2" required>
 
-      <div class="form_group">
-
-        <label><input type="radio" name="radi" id="plus" value="+">+</label>
-          
-      </div>
-
-      <div class="form_group">
-
-         <label><input type="radio" name="radi" id="mainus" value="-">-</label>
-    
-      </div>
-
-      <div class="form_group">
-          <label><input type="radio" id="multiple" name="radi" value="*">*</label>
-         
-      </div>
-
-      <div class="form_group">
-         <label><input type="radio" name="radi" id="division" value="/">/</label>
-         
-      </div>
-       <br>
-      <input type="submit" value="send" name="send" class="btn">
-
+      <button type="submit">Calculer</button>
    </form>
 
-   
+   <br>
+
+
    
 </body>
 </html>
-
-
-<?php   
- 
- //started projects
- if(isset($_POST["send"])){
-  
-   function calculator(){
-       
-
-      if(!isset($_POST["radi"])){
-         echo "<p class='radio_'>please choose an operation </p>";
-         
-         return;
-       }
-
-      elseif($_POST["number_one"]==""){
-           echo "<p class='value_A'>number one is required</p>";
-           return;
-       }
-      
-       elseif($_POST["number_tow"] ==""){
-          echo "<p class='value_A'>number tow is required </p>";
-          return;
-       }
-
-      //  create to variable to add numbers
-       $number_one=$_POST["number_one"];
-       $number_tow=$_POST["number_tow"];
-      
-      //create switch to click user any radio to calculate
-      switch($_POST["radi"]){
-
-         case "+":
-           echo "<p class='totla_number'>" . ($number_one + $number_tow) . "</p>";
-         break;
-          
-         case "-":
-              echo "<p class='totla_number'>" . $number_one - $number_tow . "</p>";
-         break;
-
-         case "*":
-             echo "<p class='totla_number'>". $number_one * $number_tow ."</p>";
-         break;
-  
-          case "/":
-             
-             if( $number_tow == 0){
-                 echo "division by zero not allowed";
-             }else{
-                echo "<p class='totla_number'>". $number_one / $number_tow ."</p>";
-             }
-         break;
-
-         default :
-            echo "you not chose any value";
-            
-       };
-        
-   }
-
-   //call function
-   calculator();
-
-
- }
- 
-
-?>
